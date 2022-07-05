eea96b22f92f2916e2e817b07beee321646ed153
TedTinker <tedjtinker@gmail.com>
2022-07-05 05:04:38 -0700

    working

--- train_test.py
+++ train_test.py
@@ -30,7 +30,7 @@ def train_test(model, epochs = 100, batch_size = 128, show_after = 100):
             accurate = [True if torch.argmax(p).item() == y[i].item() else False for i, p in enumerate(predicted)]
             test_acc.append(100*sum(accurate)/len(accurate))
 
-            if(e%show_after == 0 or e==1 or e==epochs):
+            if(e%show_after == 0 or e==epochs):
                 plot_losses(model, e, train_losses, test_losses)
                 plot_accuracy(model, e, train_acc, test_acc)
                 #save_model(model, e)
